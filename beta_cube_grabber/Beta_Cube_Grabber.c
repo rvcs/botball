01da2806510c9d8b3c6ab8901e736f376f23303a
Builderdude512 <Builderdude512@gmail.com>
2014-03-24 10:21:28 -0700

    merged camera program with my program

--- beta_cube_grabber/Beta_Cube_Grabber.c
+++ beta_cube_grabber/Beta_Cube_Grabber.c
@@ -1,12 +1,33 @@
 // Created on Sat March 15 2014
 
+#define RIGHT_MOTOR 1
+#define LEFT_MOTOR 0
+#define ARM_UP 900
+#define ARM 0
+#define TOO_FAR_LEFT 55
+#define TOO_FAR_RIGHT 85
+#define CORRECT_HEIGHT 90
+#define CLAW 1
+#define CLAW_OPEN 700
+#define CLAW_CLOSED 200
+
 int main()
 {
+	//initalise sensors and servos and stuff
+	printf("Hello, World!\n");
 	enable_servos ();
+	
+	camera_open();
+	camera_update();
+	msleep(300);
+	printf("initialised hardware\n");
+	
 	closearm ();
 	elbowdrop ();
 	
 	move_to_shelf();
+	
+	//move_to_shelf_w_camera();
 	
 	//insert cube finding protocall
 	
@@ -26,6 +47,80 @@ int move_to_shelf()
 	//try to get alligned with the board
 	move (600, 450, 3000);
 	
+	return 1;
+}
+
+int move_to_shelf_w_camera()
+{
+	int objseen = 0;
+	int lcube = 0;
+	int rcube = 0;
+	
+	point2 cubecenter1;
+	point2 cubecenter2;
+	
+	objseen = get_object_count(0);
+	
+	/**
+	 *	shouldn't call get object center (0,0) if objseen == 0,
+	 *	there is no object number 0 in this case.
+	 *
+	 *	shouldn't call get object center (0,1) if objseen < 2,
+	 *	there is no object number 1 in this case.
+	 */
+	
+	cubecenter1 = get_object_center(0,0);
+	cubecenter2 = get_object_center(0,1);
+	
+	if(cubecenter1.x > cubecenter2.x)
+	{
+		rcube = cubecenter1.x;
+		lcube = cubecenter2.x;
+	}
+	else 
+	{
+		lcube = cubecenter1.x;
+		rcube = cubecenter2.x;
+	}
+	/**
+	 *	above, the code checks for which of two objects is the left one, but
+	 *	the idea of more than one object is not present inside this while loop.
+	 *
+	 *	can't call object get object center 0,0 unless you know you have an object
+	 *	like above.
+	 */
+
+	while(cubecenter1.y <= CORRECT_HEIGHT)
+	{
+		camera_update();
+		
+		cubecenter1 = get_object_center(0,0);
+		
+		if(cubecenter1.x >= TOO_FAR_LEFT && cubecenter1.x <= TOO_FAR_RIGHT)
+		{
+			mav(RIGHT_MOTOR, 600);
+			mav(LEFT_MOTOR, 600);
+			
+		}
+		if(cubecenter1.x <= TOO_FAR_LEFT)
+		{
+			
+			mav(RIGHT_MOTOR, 700);
+			mav(LEFT_MOTOR, 300);
+			
+		}
+		if(cubecenter1.x >= TOO_FAR_RIGHT)
+		{
+			
+			mav(RIGHT_MOTOR, 300);
+			mav(LEFT_MOTOR, 700);
+			
+		}
+		/**
+		 *	I think that the orange object is going to go off screen 
+		 *	before it's going to be in range of the grabber.
+		 */
+	}
 	return 1;
 }
 
