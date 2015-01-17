08270d01c39b496cffa717b3c8e8fe284eb19270
Jordan <612stardancer@gmail.com>
2015-01-17 13:49:22 -0800

    made robot turn in a square

---
+++ workshop2015/robot.c
@@ -0,0 +1,50 @@
+// Created on Sat January 17 2015 hi
+
+int main()
+{
+	printf("Hello, World!\n");
+	msleep(2500);
+	printf("jordan\n");
+	
+	// make the robot move for two seconds or something like that
+	motor(0, 80);
+	motor(3, 80);
+	msleep(2000);
+	
+	// make the robot move for two seconds or something like that
+	motor(0, 80);
+	motor(3, 0);
+	msleep(2000);
+	
+	// make the robot move for two seconds or something like that
+	motor(0, 80);
+	motor(3, 80);
+	msleep(2000);
+	
+		// make the robot move for two seconds or something like that
+	motor(0, 80);
+	motor(3, 0);
+	msleep(2000);
+		// make the robot move for two seconds or something like that
+	motor(0, 80);
+	motor(3, 80);
+	msleep(2000);
+	
+	// make the robot move for two seconds or something like that
+	motor(0, 80);
+	motor(3, 0);
+	msleep(2000);
+	
+	// make the robot move for two seconds or something like that
+	motor(0, 80);
+	motor(3, 80);
+	msleep(2000);
+	
+		// make the robot move for two seconds or something like that
+	motor(0, 80);
+	motor(3, 0);
+	msleep(2000);
+	
+	ao();
+	return 0;
+}
