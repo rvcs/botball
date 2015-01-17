95321c46de08ffbcd05d5c88ed27fb3498843c13
Jordan <612stardancer@gmail.com>
2015-01-17 14:55:47 -0800

    refractor

--- workshop2015/robot.c
+++ workshop2015/robot.c
@@ -1,50 +1,41 @@
 // Created on Sat January 17 2015 hi
-
+void drive_forward(int power);
+void turn_right(int power);
+void driveand_turnright(int power);
 int main()
 {
 	printf("Hello, World!\n");
 	msleep(2500);
 	printf("jordan\n");
 	
-	// make the robot move for two seconds or something like that
-	motor(0, 80);
-	motor(3, 80);
-	msleep(2000);
+	driveand_turnright(80);
 	
-	// make the robot move for two seconds or something like that
-	motor(0, 80);
-	motor(3, 0);
-	msleep(2000);
+	driveand_turnright(80);
 	
-	// make the robot move for two seconds or something like that
-	motor(0, 80);
-	motor(3, 80);
-	msleep(2000);
+	driveand_turnright(80);
 	
-		// make the robot move for two seconds or something like that
-	motor(0, 80);
-	motor(3, 0);
-	msleep(2000);
-		// make the robot move for two seconds or something like that
-	motor(0, 80);
-	motor(3, 80);
-	msleep(2000);
-	
-	// make the robot move for two seconds or something like that
-	motor(0, 80);
-	motor(3, 0);
-	msleep(2000);
-	
-	// make the robot move for two seconds or something like that
-	motor(0, 80);
-	motor(3, 80);
-	msleep(2000);
-	
-		// make the robot move for two seconds or something like that
-	motor(0, 80);
-	motor(3, 0);
-	msleep(2000);
-	
+	driveand_turnright(80);
+
 	ao();
 	return 0;
+}
+//end of main function
+void driveand_turnright(int power)
+{
+	drive_forward(power);
+	turn_right(power);
+}
+void drive_forward(int power)
+{
+	// make the robot move for two seconds or something like that
+	motor(0, power);
+	motor(3, power);
+	msleep(2000);	
+}
+void turn_right(int power)
+{
+		// make the robot move for two seconds or something like that
+	motor(0, power);
+	motor(3, 0);
+	msleep(2000);
 }
