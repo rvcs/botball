53fd79382587defa8f78bde67697de8e96e989a5
Jordan <612stardancer@gmail.com>
2015-03-18 20:10:15 -0700

    changes

--- 2015/sweep gold poms/sweep gold poms.c
+++ 2015/sweep gold poms/sweep gold poms.c
@@ -1,4 +1,11 @@
 // Created on Mon March 16 2015
+void turn_right()
+{	
+	motor(0,80);
+	motor(2,0);
+	msleep(1270);
+}
+
 int main()
 {
 	double start = 0.0;
@@ -6,7 +13,7 @@ int main()
 	// if you are using a Create you should uncomment the next line
 	//create_connect();
 	enable_servos ();
-	set_servo_position(3,1706);
+	set_servo_position(3,1977);
 	
 	
 	wait_for_light(2); // change the port number to match where your robot 
@@ -37,45 +44,76 @@ int main()
 	motor(0,-100);
 	motor(2,-100);
 	msleep(2000);
+	
 	  //go forwad 
 	motor(0,85);
 	motor(2,85);
 	//msleep(6000);
 	printf("before 3000 delay\n");
 	msleep (3000);
+	set_servo_position(3,1706);
 	printf("after 3000 delay\n");
 	printf("%d\n",analog(1));
+	start=seconds ();
+
 	while ((a1 = analog(1)) <500) {
 		printf("%d\n",a1);
 		msleep(5);
+		if(seconds()-start>3.5){
+			break;
+		}
 	}
 	printf("Found black %d\n", a1);
-	start=seconds ();
-	//turn to gointo middle
-	motor(0,80);
-	motor(2,0);
-	msleep(1250);
+
+	//turn to go into middle
+	turn_right();
 	
 //go forward	
 	motor(0,85);
 	motor(2,85);
 	printf("go forward\n");
 	//msleep(2000);
-	while ((a1 = analog(1)) <500 || (a2 = analog(3)) <500) {
+	while ((a1 = analog(1)) <600 || (a2 = analog(3)) <600) {
 		printf("%4d %4d\n",a1, a2);
-		msleep(5);
-		if (seconds()-start >7.0) {
+		//msleep(5);
+		if (seconds()-start >8.5) {
 			break;
 		}
 	}
 	printf("%4d %4d\n",a1, a2);
+
+	//turn in cave
+	//turn_right()0;
+	motor(0,0);
+	motor(2,0);
+	clear_motor_position_counter(0);
+	mtp(0, 1500, 1300);
+	block_motor_done(0);
+
+	set_servo_position(3,1977);
 	//go to pick up cubes
 	//motor(0,75);
 	//motor(2,0);
 	printf("turn right\n");
 	//msleep(1500);
 	
-	
+	//line follow
+	start=seconds ();
+	while(1){
+		if(seconds()-start>5.0){
+			break;
+		}
+		if (analog(1)>500) {
+			motor(0,80);
+			motor(2,100);
+		}else if (analog(3)>500) {
+			motor(0,100);
+			motor(2,80);
+		}else {
+			motor(0,100);
+			motor(2,100);
+		}
+	}
 	
 	//motor(0,85);
 	//motor(2,85);
@@ -83,10 +121,28 @@ int main()
 //put arm downto grab
 	set_servo_position(3,633);	
 	msleep(1000);
+	start=seconds ();
+	while(1){
+		if(seconds()-start>10.0){
+			break;
+		}
+		if (analog(1)>500) {
+			motor(0,-80);
+			motor(2,-100);
+		}else if (analog(3)>500) {
+			motor(0,-100);
+			motor(2,-80);
+		}else {
+			motor(0,-100);
+			motor(2,-100);
+		}
+	}
+
+	//motor(0,-75);
+	//motor(2,-75);
+	//msleep(8500);
 	
-	motor(0,-75);
-	motor(2,-75);
-	msleep(1000);
+turn_right();
 	ao();
 	return 0;
 }
