0b186e3ad258a913b3ea8a1863734f0f57dd7683
mwsmith98 <mwsmith100@gmail.com>
2016-03-05 12:07:52 -0800

    Update irobot2016.c

--- irobot2016.c
+++ irobot2016.c
@@ -1,7 +1,11 @@
+
+#define SPIN 157
+#define FORWARD 145
+#define DISTANCE 156
 char opcodes[1024];
 char * opcodes_tail = NULL; //creates system to avoid having to count bytes
 
-void write_byte(b)
+void write_byte(int b)
 {
   *opcodes_tail = b;
   opcodes_tail++;
@@ -15,21 +19,21 @@ int num_bytes()
 int main()
 {
 	int i = 0;
-	short distance1 = -575; 
-	short distance2 = -400;
-	short distance3 = -560;
+	short distance1 = -560; 
+	short distance2 = -405;
+	short distance3 = -470;
+	short distance4 = 460;
+	short distance5 = 1580;
 	short speed1 = -500; //Variable 'speed' equal to value
-	short speed2 = 300;
-	short speed3 = -200;
-	short speed4 = 200;
-	short speed5 = -250;
-	short speed6 = 250;
-	short angle1 = -125;
-	short angle2 = 70;
+	short speed2 = 500;
+	short speed3 = 100;
+	short angle1 = 70;
+	short angle2 = -60;
 	short angle3 = -20;
+	short angle4 = -175;
+	short angle5 = -90;
 	opcodes_tail = opcodes;
 	
-	printf("BACON!\n");
 	create_connect();
 	msleep(2000);
 	printf("Connected\n");
@@ -39,57 +43,88 @@ int main()
 	
 	write_byte(158); //wait for:
 	write_byte(7); //right bumb 1 byte
-
-	write_byte(145); //go forward mm/s
+	
+//movement begins
+	write_byte(FORWARD); //go forward mm/s
 	write_byte(speed1>>8); 
 	write_byte(speed1&0xff); 
-	write_byte(speed1>>8); 
-	write_byte(speed1&0xff); 
 
-	write_byte(156); //count distance for [distance1]
+	write_byte(DISTANCE); //count distance for [distance1]
 	write_byte(distance1>>8); 
 	write_byte(distance1&0xff);	
 	
-	write_byte(145);// spin to the left
-	write_byte(speed4>>8);
-	write_byte(speed4&0xff);
-	write_byte(speed3>>8);
-	write_byte(speed3&0xff);
-
-	write_byte(157); 
-	write_byte(angle2>>8);
-	write_byte(angle2&0xff);
+	write_byte(SPIN); //left
+	write_byte(angle1>>8);
+	write_byte(angle1&0xff);
 	
-	write_byte(145); //go forward mm/s
-	write_byte(speed1>>8); 
-	write_byte(speed1&0xff); 
-	write_byte(speed1>>8); 
+	write_byte(FORWARD);
+	write_byte(speed1>>8);
 	write_byte(speed1&0xff); 
 
-	write_byte(156); //count distance for [distance1]
-	write_byte(distance3>>8); 
-	write_byte(distance3&0xff);	
+	write_byte(DISTANCE); //count distance for [distance1]
+	write_byte(distance2>>8); 
+	write_byte(distance2&0xff);	
 	
-	write_byte(145);// spin to the right
-	write_byte(speed5>>8);
-	write_byte(speed5&0xff);
-	write_byte(speed6>>8);
-	write_byte(speed6&0xff);
-
-	write_byte(157); 
+	write_byte(SPIN); //spin right
 	write_byte(angle3>>8);
 	write_byte(angle3&0xff);
 	
-	write_byte(145); //go forward mm/s
-	write_byte(speed1>>8); 
-	write_byte(speed1&0xff); 
-	write_byte(speed1>>8); 
+	write_byte(FORWARD);
+	write_byte(speed1>>8);
 	write_byte(speed1&0xff); 
 
-	write_byte(156); //count distance for [distance2]
+	write_byte(DISTANCE); //count distance for [distance2]
 	write_byte(distance2>>8); 
 	write_byte(distance2&0xff);	
 
+	write_byte(SPIN);//spin left
+	write_byte(angle1>>8);
+	write_byte(angle1&0xff);
+	
+	write_byte(FORWARD); //go forward mm/s
+	write_byte(speed1>>8);
+	write_byte(speed1&0xff);
+	
+	write_byte(DISTANCE);//count distance1
+	write_byte(distance1>>8);
+	write_byte(distance1&0xff);
+	
+	write_byte(SPIN);//spin right
+	write_byte(angle1>>8);
+	write_byte(angle1&0xff);
+	
+	write_byte(FORWARD);//go foward mm/s
+	write_byte(speed1>>8);
+	write_byte(speed1&0xff);
+	
+	write_byte(DISTANCE);//go distance
+	write_byte(distance1>>8);
+	write_byte(distance1&0xff);
+	
+	write_byte(SPIN);//spin right
+	write_byte(angle4>>8);
+	write_byte(angle4&0xff);
+	
+	write_byte(FORWARD);//go forward mm/s
+	write_byte(speed1>>8);
+	write_byte(speed1&0xff);
+	
+	write_byte(DISTANCE);//distance 
+	write_byte(distance5>>8);
+	write_byte(distance5&0xff);
+	
+	write_byte(SPIN);//spin right
+	write_byte(angle1>>8);
+	write_byte(angle1&0xff);
+	
+	write_byte(FORWARD);//go forward
+	write_byte(speed2>>8);
+	write_byte(speed2&0xff);
+	
+	write_byte(DISTANCE);//distance
+	write_byte(distance1>>8);
+	write_byte(distance1&0xff);
+	
 	write_byte(7); //reboot 1 byte
 
 	msleep(500);
