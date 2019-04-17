9f7fa591f740dd5d8c181718c476a529fed2c0cb
Brian C Sparks <brian.c.sparks@hp.com>
2019-04-17 13:24:19 -0700

    Slow down at end

--- 2019/shield/CenterClear/main.c
+++ 2019/shield/CenterClear/main.c
@@ -9,17 +9,17 @@ int main()
     //code
     msleep(1000);
     create_drive_direct(-150,150);
-    msleep(1850);
+    msleep(1900);
     create_drive_direct(-150, -150);
     msleep(4500);
     create_drive_direct(150,-150);
-    msleep(1850);
+    msleep(1900);
     create_drive_direct(-150,-150);
     msleep(7500);
-    create_drive_direct(150,-150);
-    msleep(2000);
+    create_drive_direct(-25,-150);
+    msleep(6000);
     create_drive_direct(-100,-100);
-    msleep(3500);
+    msleep(2000);
     
     
    create_disconnect();
