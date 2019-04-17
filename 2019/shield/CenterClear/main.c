852c65cdd5c2f58eaa2d7a6bbc5f17e72196c6be
Brian C Sparks <brian.c.sparks@hp.com>
2019-04-17 13:25:10 -0700

    Arm to move blue poms off tape

--- 2019/shield/CenterClear/main.c
+++ 2019/shield/CenterClear/main.c
@@ -7,6 +7,8 @@ int main()
    shut_down_in(119);
     
     //code
+    enable_servos();
+    set_servo_position(0,1900);
     msleep(1000);
     create_drive_direct(-150,150);
     msleep(1900);
@@ -14,15 +16,18 @@ int main()
     msleep(4500);
     create_drive_direct(150,-150);
     msleep(1900);
+    set_servo_position(0,200);
     create_drive_direct(-150,-150);
-    msleep(7500);
+    msleep(7750);
+    set_servo_position(0,2020);
     create_drive_direct(-25,-150);
     msleep(6000);
     create_drive_direct(-100,-100);
-    msleep(2000);
+    msleep(2250);
     
     
    create_disconnect();
+   ao();
    return 0;
 }
 
