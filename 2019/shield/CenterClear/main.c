4e49e13870bd558b6c91006d49dd6de8ccaa4fdb
Brian C Sparks <brian.c.sparks@hp.com>
2019-04-17 13:23:05 -0700

    added wait_for_light

--- 2019/shield/CenterClear/main.c
+++ 2019/shield/CenterClear/main.c
@@ -3,18 +3,19 @@
 int main()
 {
    create_connect();
-   //wait_for_light(0);
+   wait_for_light(0);
    shut_down_in(119);
     
     //code
+    msleep(1000);
     create_drive_direct(-150,150);
-    msleep(2000);
+    msleep(1850);
     create_drive_direct(-150, -150);
-    msleep(5000);
-    create_drive_direct(150,-150);a
-    msleep(1975);
+    msleep(4500);
+    create_drive_direct(150,-150);
+    msleep(1850);
     create_drive_direct(-150,-150);
-    msleep(8000);
+    msleep(7500);
     create_drive_direct(150,-150);
     msleep(2000);
     create_drive_direct(-100,-100);
